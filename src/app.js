ab932cada2b00861215ea171c1b622d8b5152a87
carolgreene <carolgreene3@gmail.com>
2020-06-11 18:15:44 -0700

    added update functionality

--- src/app.js
+++ src/app.js
@@ -13,6 +13,9 @@ document.querySelector('#posts').addEventListener('click', deletePost);
 //Listen for edit state
 document.querySelector('#posts').addEventListener('click', enableEdit);
 
+//Listen for cancel
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
+
 //get posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
@@ -24,21 +27,43 @@ function getPosts() {
 function submitPost() {
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
+  const id = document.querySelector('#id').value;
 
-  //use es2015 where is key & value are the same, don't need to list both(below for title & body)
-  const data = {
+   //use es2015 where is key & value are the same, don't need to list both(below for title & body)
+   const data = {
     title,
     body
-  }
+   }
 
-  //Create post
-  http.post('http://localhost:3000/posts', data)
-  .then(data => {
-    ui.showAlert('Post added', 'alert alert-success')  //arguments are the msg & the class where it goes
-    ui.clearFields();
-    getPosts();   //call getPosts() so u see all posts including the 1 u just added
-  })
-  .catch(err => console.log(err));
+  //validate input
+  if(title === '' || body === '') {
+    ui.showAlert('Please fill in all fields', 'alert alert-danger');
+  } else {   
+    //check for ID
+    if(id === '') {
+      //Create Post
+      http.post('http://localhost:3000/posts', data)
+      .then(data => {
+        ui.showAlert('Post added', 'alert alert-success')  //arguments are the msg & the class where it goes
+        ui.clearFields();
+        getPosts();   //call getPosts() so u see all posts including the 1 u just added
+      })
+      .catch(err => console.log(err));
+    } else {
+      //Update Post
+      http.put(`http://localhost:3000/posts/${id}`, data)
+      .then(data => {
+        ui.showAlert('Post updated', 'alert alert-success')  //arguments are the msg & the class where it goes
+        ui.changeFormState('add');
+        getPosts();   //call getPosts() so u see all posts including the 1 u just added
+      })
+      .catch(err => console.log(err));
+    }
+    
+  
+    //Create post
+    
+  }
 }
 
  //Delete post
@@ -74,6 +99,14 @@ function submitPost() {
 
      //Fill form with the current post to be edited
      ui.fillForm(data);
+   }
+   e.preventDefault()
+ }
+
+ //Cancel Edit State
+ function cancelEdit(e) {
+   if(e.target.classList.contains('post-cancel')) {
+     ui.changeFormState('add');
    }
    e.preventDefault()
  }
--- src/ui.js
+++ src/ui.js
@@ -78,6 +78,45 @@ class UI {
     this.titleInput.value = data.title;
     this.bodyInput.value = data.body;
     this.idInput.value = data.id;
+    //call changeFormState method
+    this.changeFormState('edit');
+  }
+
+  //Clear ID hidden value
+  clearIdInput() {
+    this.idInput.value = '';
+  }
+
+
+  //Change the form state
+  changeFormState(type) {
+    if(type === 'edit') {
+      this.postSubmit.textContent = 'Update Post';
+      this.postSubmit.className = 'post-submit btn btn-warning btn-block';
+
+      //Create cancel button
+      const button = document.createElement('button');
+      button.className = 'post-cancel btn btn-light btn-block';
+      button.appendChild(document.createTextNode('Cancel Edit'));
+
+      //get parent
+      const cardForm = document.querySelector('.card-form');
+      //get element to insert before
+      const formEnd = document.querySelector('.form-end')
+      //insert cancel button
+      cardForm.insertBefore(button, formEnd)
+    } else {
+      this.postSubmit.textContent = 'Post It';
+      this.postSubmit.className = 'post-submit btn btn-primary btn-block';
+      //Remove cancel  btn if it is there
+      if(document.querySelector('post-cancel')) {
+        document.querySelector('post-cancel').remove();
+      }
+      //Clear ID from hidden field
+      this.clearIdInput()
+      //Clear text
+      this.clearFields();
+    }
   }
 }
 
